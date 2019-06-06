ce292d241de4b9bf319919d6ccc1953d737a5ff2
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-06-06 11:53:02 +0100

    practice using state

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
-// your Bomb code here!
+import React from 'react'
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            secondsLeft: this.props.initialCount
+        }
+    }
+    render() {
+        return (
+            (this.state.secondsLeft === 0)
+            ? <p>Boom!</p>
+            : <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+   
+        )
+    }
+}
+
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
-// your ImageSlider code here!
+import React from 'react'
+
+export default class ImageSlider extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            initialCount: 0,
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+        return (
+        <div>
+            <p>I am on slide {this.state.currentSlideIndex}</p>
+        </div>
+        )
+    }
+}
